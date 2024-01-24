08e7cdc4ec67ae13a80096e59531e3ba48b87a28
jahir hussain <jahir.hussain.14268.jh@gmail.com>
2024-01-25 02:18:44 +0530

    Completed skills, experience and project section

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function showsidebar(){
 function hidesidebar(){
     side.style.display = "none";
 }
+
+
+// Mouse Hover for project transition
+var element = document.querySelector('.project-details');
+
+// Add event listeners for mouseover and mouseout
+element.addEventListener('mouseover', function() {
+// Code to run when the mouse enters the element
+element.classList.add('project-transition');
+console.log('Mouse over!');
+    // You can also add additional styling or actions here
+
+});
+
+element.addEventListener('mouseout', function() {
+    // Code to run when the mouse leaves the element
+    console.log('Mouse out!');
+    element.classList.remove('project-transition');
+    // You can also add additional styling or actions here
+});
