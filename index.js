805eb8e4a9836b42eaa2c9b19a924d0d57189825
jahir hussain <jahir.hussain.14268.jh@gmail.com>
2024-01-25 13:34:17 +0530

    Completed all the sections

--- index.js
+++ index.js
@@ -9,21 +9,32 @@ function hidesidebar(){
 }
 
 
-// Mouse Hover for project transition
-var element = document.querySelector('.project-details');
+// // Mouse Hover for project transition
+// var element = document.querySelector('.project-details');
 
-// Add event listeners for mouseover and mouseout
-element.addEventListener('mouseover', function() {
-// Code to run when the mouse enters the element
-element.classList.add('project-transition');
-console.log('Mouse over!');
-    // You can also add additional styling or actions here
+// // Add event listeners for mouseover and mouseout
+// element.addEventListener('mouseover', function() {
+// // Code to run when the mouse enters the element
+// element.classList.add('project-transition');
+// console.log('Mouse over!');
+//     // You can also add additional styling or actions here
 
-});
+// });
 
-element.addEventListener('mouseout', function() {
-    // Code to run when the mouse leaves the element
-    console.log('Mouse out!');
-    element.classList.remove('project-transition');
-    // You can also add additional styling or actions here
-});
+// element.addEventListener('mouseout', function() {
+//     // Code to run when the mouse leaves the element
+//     console.log('Mouse out!');
+//     element.classList.remove('project-transition');
+//     // You can also add additional styling or actions here
+// });
+
+// Year update in footer
+
+function updateYear() {
+    var currentYear = new Date().getFullYear();
+
+    document.getElementById('liveYear').textContent = currentYear;
+}
+
+// Initial update
+updateYear();
